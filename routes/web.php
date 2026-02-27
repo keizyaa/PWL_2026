377d4eb2caecf3d2bd91b847a2dba14e30c3358a
Tsabita Keizya Vanya Putri <tsabitakeizya@gmail.com>
2026-02-27 20:22:24 +0700

    PWL_Jb2_Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,33 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat datang';
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return '244107020183 | Tsabita Keizya Vanya Putri';
+});
+
+Route::get('/user/{keizya}', function($keizya) {
+    return 'Nama saya '.$keizya;
+});
+
+Route::get('/posts/{posts}/comment/{comment}', function($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-".$commentId;
+});
+
+Route::get('/articles/{articles}', function($articlesId) {
+    return 'Halaman Artikel dengan Id '.$articlesId;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
 });
